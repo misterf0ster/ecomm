de4c9655c0d47a00a7af85396873c9dbc886c9dd
misterf0ster <mrf0ster@yahoo.com>
2025-05-03 22:10:17 +0300

    add pkg

---
+++ pkg/db/db.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/jackc/pgx/v5"
+)
+
+type DB struct {
+	DB *pgx.Conn
+}
+
+// Открываю коннект
+func Open(url string) (*DB, error) {
+	conn, err := pgx.Connect(context.Background(), url)
+	if err != nil {
+		return nil, fmt.Errorf("database connection error: %w", err)
+	}
+
+	if err := conn.Ping(context.Background()); err != nil {
+		conn.Close(context.Background())
+		return nil, fmt.Errorf("error checking database connection: %w", err)
+	}
+
+	log.Println("successfully connected to database")
+	return &DB{DB: conn}, nil
+}
+
+// Закрываю соединение
+func (db *DB) Close() {
+	if db.DB != nil {
+		db.DB.Close(context.Background())
+		log.Println("Closed db connection")
+	}
+}
