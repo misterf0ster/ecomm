9f5a1e166c140a1f00cb77424f1e48ae3a867c9d
misterf0ster <mrf0ster@yahoo.com>
2025-05-03 22:13:28 +0300

    add jwt

--- pkg/token/jwt.go
+++ pkg/token/jwt.go
@@ -1,1 +1,47 @@
 package token
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+var jwtSecret = []byte("qwert") // Секретный ключ для JWT
+
+// Генерация Access токена
+func GetAccessToken(userID, ip string) (string, error) {
+	claims := jwt.MapClaims{
+		"user_id": userID,
+		"ip":      ip,
+		"exp":     time.Now().Add(1 * time.Hour).Unix(), // Срок жизни - 1 час
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, claims)
+	accessToken, err := token.SignedString(jwtSecret)
+	if err != nil {
+		return "", fmt.Errorf("could not sign access token: %w", err)
+	}
+
+	return accessToken, nil
+}
+
+// Парсинг Access токена
+func ParseAccessToken(tokenString string) (jwt.MapClaims, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Проверка алгоритма подписи
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+		return jwtSecret, nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("could not parse token: %w", err)
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+	return claims, nil
+}
